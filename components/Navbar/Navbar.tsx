86535f216239fb97b64f895025ba92ffa3725599
Nikita <pozhidaevw@gmail.com>
2023-07-03 14:43:24 +0700

    add: adaptive left section navbar with use: useRef, useEffect and so on

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 // Components Navbar
 import NavbarSearch from "./NavbarSearch";
@@ -15,6 +15,7 @@ import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
 
 // Icons Mui
 import AppsIcon from "@mui/icons-material/Apps";
+import DensityMediumIcon from "@mui/icons-material/DensityMedium";
 
 type Props = {
   mode: boolean;
@@ -29,37 +30,72 @@ export default function Navbar({ mode, setMode }: Props) {
     setOpen(!open);
   };
 
+  // Adaptive navbar
+  const navbarRef = useRef<HTMLElement>(null);
+  const [size, setSize] = useState<any>({});
+
+  const resizeHandler = () => {
+    const { clientHeight, clientWidth } = navbarRef.current || {};
+    setSize({ clientHeight, clientWidth });
+  };
+
+  useEffect(() => {
+    console.log(navbarRef.current?.offsetWidth);
+    window.addEventListener("resize", resizeHandler);
+    resizeHandler();
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }} ref={navbarRef}>
       <AppBar position="static" sx={{ height: 50, boxShadow: 0 }}>
         <Toolbar sx={{ mt: -0.85 }}>
           <Typography sx={{ flexGrow: 1 }}>
-            <NavbarLogo />
-            <NavbarMenuItem text="Рабочие пространства" assignment="work" />
-            <NavbarMenuItem text="Недавние" assignment="recent">
-              <NavbarSubmenuItem />
-              <NavbarSubmenuItem />
-            </NavbarMenuItem>
-            <NavbarMenuItem text="В избранном" assignment="favorite" />
-            <NavbarMenuItem text="Шаблоны" assignment="favorite">
-              <NavbarSubmenuTemplates />
-            </NavbarMenuItem>
-            <Button
-              aria-label="create border"
-              sx={{
-                color: "inherit",
-                minWidth: 5,
-                px: "14px",
-                py: "3.5px",
-                ml: "5px",
-                backgroundColor: "#23bf0b",
-                textTransform: "none",
-                fontSize: 14,
-                ":hover": { backgroundColor: "#4cf233" },
-              }}
-            >
-              Создать
-            </Button>
+            {size.clientWidth >= 1100 ? (
+              <>
+                <NavbarLogo />
+                <NavbarMenuItem text="Рабочие пространства" assignment="work" />
+                <NavbarMenuItem text="Недавние" assignment="recent">
+                  <NavbarSubmenuItem />
+                  <NavbarSubmenuItem />
+                </NavbarMenuItem>
+                <NavbarMenuItem text="В избранном" assignment="favorite" />
+                <NavbarMenuItem text="Шаблоны" assignment="favorite">
+                  <NavbarSubmenuTemplates />
+                </NavbarMenuItem>
+                <Button
+                  aria-label="create border"
+                  sx={{
+                    color: "inherit",
+                    minWidth: 5,
+                    px: "14px",
+                    py: "3.5px",
+                    ml: "5px",
+                    backgroundColor: "#23bf0b",
+                    textTransform: "none",
+                    fontSize: 14,
+                    ":hover": { backgroundColor: "#4cf233" },
+                  }}
+                >
+                  Создать
+                </Button>
+              </>
+            ) : (
+              <Button
+                aria-label="create border"
+                sx={{
+                  color: "inherit",
+                  minWidth: 5,
+                  px: "14px",
+                  py: "3.5px",
+                  ml: "5px",
+                }}
+              >
+                <DensityMediumIcon />
+              </Button>
+            )}
           </Typography>
           <NavbarSearch />
           <Button
