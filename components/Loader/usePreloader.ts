bfd4f5ea19143bc469f8f03e8066ac624f98641a
Nikita <pozhidaevw@gmail.com>
2023-08-29 22:08:18 +0700

    update: favicon, preloader

--- components/Loader/usePreloader.ts
+++ components/Loader/usePreloader.ts
@@ -4,7 +4,7 @@ export function usePreloader() {
   const [loader, setLoader] = useState<boolean>(true);
 
   useEffect(() => {
-    setInterval(() => setLoader(false), 1500);
+    setInterval(() => setLoader(false), 1000);
   }, []);
 
   return { loader };
