d79bc5c98563876cedb5de7c3ba7df85ccf6eebe
Nikita <pozhidaevw@gmail.com>
2023-09-08 15:59:53 +0700

    update: Card Board

--- components/Boards/BoardItem.tsx
+++ components/Boards/BoardItem.tsx
@@ -1,43 +1,46 @@
 "use client";
 
-import Image from "next/image";
+import { useState } from "react";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Rating, keyframes } from "@mui/material";
 
 import { styled, alpha } from "@mui/material/styles";
 
+const AnimationBoard = keyframes`
+from {
+  background: rgba(0,0,0,0)
+}
+to {
+  background: rgba(0,0,0,0.25)
+}`;
+
 const ContainerBoardItem = styled(Box)(({ theme }) => ({
   position: "relative",
-  width: "240px",
-  height: "140px",
+  width: "220px",
+  height: "125px",
   borderRadius: "10px",
   backgroundColor: alpha(theme.palette.text.primary, 0.15),
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
   padding: "10px",
+  cursor: "pointer",
+  boxShadow: theme.shadows[3],
   "&:hover::before": {
     content: "''",
     position: "absolute",
-    borderRadius: "10px",
     top: 0,
     bottom: 0,
     right: 0,
     left: 0,
+    borderRadius: "10px",
     zIndex: 1,
-
-    display: "block",
-    backgroundImage:
-      "url(https://images.unsplash.com/photo-1652874131919-2b85d7dbc2c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2072&q=80)",
-    backgroundSize: "cover",
-    width: "100%",
-    height: "100%",
-
-    cursor: "pointer",
-    filter: "blur(0.75px)",
+    animation: `${AnimationBoard} 0.4s`,
+    animationFillMode: "forwards",
   },
 }));
 
 const NameBoard = styled(Typography)(({ theme }) => ({
+  color: "white",
   display: "-webkit-box",
   position: "absolute",
   width: "91%",
@@ -47,7 +50,18 @@ const NameBoard = styled(Typography)(({ theme }) => ({
   WebkitLineClamp: 2,
   WebkitBoxOrient: "vertical",
   textOverflow: "ellipsis",
-  zIndex: 1,
+  zIndex: 2,
+  fontSize: "16px",
+  fontWeight: "700",
+}));
+
+const RatingCustom = styled(Rating)(() => ({
+  stroke: "white",
+  strokeWidth: "0.5px",
+  position: "absolute",
+  bottom: 10,
+  right: 14,
+  zIndex: 2,
 }));
 
 type Props = {
@@ -55,6 +69,7 @@ type Props = {
 };
 
 export default function BoardItem({ text }: Props) {
+  const [value, setValue] = useState<number | null>(null);
   return (
     <ContainerBoardItem
       sx={{
@@ -63,6 +78,14 @@ export default function BoardItem({ text }: Props) {
       }}
     >
       <NameBoard>{text}</NameBoard>
+      <RatingCustom
+        name="favorites"
+        max={1}
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
     </ContainerBoardItem>
   );
 }
