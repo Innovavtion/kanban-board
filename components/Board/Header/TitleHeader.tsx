bd3dfc932eca1fbf627e42bc654a205aeb34f804
Nikita <pozhidaevw@gmail.com>
2023-09-20 23:36:04 +0700

    update: TitleBoard simple update width input
    - Update simple width input in edit mode

--- components/Board/Header/TitleHeader.tsx
+++ components/Board/Header/TitleHeader.tsx
@@ -35,53 +35,66 @@ const TypographyTitle = styled(Typography)(({ theme }) => ({
 const InputTitle = styled(InputBase)(({ theme }) => ({
   color: alpha(theme.palette.text.primary, 0.9),
   height: "100%",
-  maxWidth: "100%",
   fontSize: "18px",
   fontWeight: "bold",
   textDecoration: "none",
-  padding: "0 8px",
+  padding: "2px 0 0 6px",
   borderRadius: "5px",
   "&.Mui-focused": {
-    boxShadow: "inset 0 0 0 2px",
+    border: `2px solid ${theme.palette.text.primary}`,
   },
 }));
 
 export default function BoardTitleHeader() {
   const titleWidth = useRef<HTMLElement>(null);
-  const [width, setWidth] = useState<number | undefined | null>(null);
+  const inputRef = useRef<HTMLElement>(null);
+
+  const [width, setWidth] = useState<number>(0);
   const [editText, setEditText] = useState<boolean>(false);
   const [text, setText] = useState<string>("Название доски");
 
-  const clickBox = () => {
+  const editOpen = () => {
     getWidth();
     setEditText(true);
   };
 
   const getWidth = () => {
-    setWidth(titleWidth.current?.clientWidth);
-    console.log(width);
+    if (titleWidth.current) setWidth(titleWidth.current.clientWidth);
+  };
+
+  const setInputWidth = (widthCurrent: number, textCurrent: string) => {
+    if (text.length < textCurrent.length) {
+      setWidth(widthCurrent + 16);
+    } else {
+      console.log("Удалил символ");
+      setWidth(widthCurrent);
+    }
   };
 
   return (
     <BoxTitleBoard
       onClick={() => {
-        clickBox();
+        editOpen();
       }}
       ref={titleWidth}
     >
       {editText ? (
-        <InputTitle
-          autoFocus
-          value={text}
-          onBlur={() => {
-            setEditText(false);
-          }}
-          onChange={(e) => {
-            setText(e.currentTarget.value);
-            getWidth();
-          }}
-          sx={{ width: { width } }}
-        />
+        <>
+          <InputTitle
+            autoFocus
+            value={text}
+            onBlur={() => {
+              setEditText(false);
+            }}
+            onChange={(e) => {
+              setText(e.currentTarget.value);
+              setInputWidth(e.currentTarget.scrollWidth, e.currentTarget.value);
+            }}
+            sx={{ width: width }}
+            ref={inputRef}
+            className="input"
+          />
+        </>
       ) : (
         <TypographyTitle>{text}</TypographyTitle>
       )}
